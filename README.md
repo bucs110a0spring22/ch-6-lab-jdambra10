0a4825a5a480629db4381995342ca7da3e3e2d52
jdambra10 <98533715+jdambra10@users.noreply.github.com>
2022-03-29 01:41:05 +0000

    finished lab

--- README.md
+++ README.md
@@ -9,17 +9,20 @@ _Replace anything surrounded by the `< >` symbols._
 
 ## SUMMARY:
  < Provide a short description of your program's functionality. No more than a couple sentences. >
+3np1 function takes user upper bound input and loops through, printing the result and loop iteration 
+
+graphData function takes upperBound input and uses that to create a line graph of the 3np1 function using its results and loop iteration value
 
 ## GRACE DAYS
-Grace days used for this assignment: < # >
+Grace days used for this assignment: < 0 >
 
-Grace days remaining: < # >/5
+Grace days remaining: < 0 >/5
 
 ## KNOWN BUGS AND INCOMPLETE PARTS:
  < What parts of the project you were not able to complete >
-
+No known bugs
 ## REFERENCES:
  < List any outside resources used >
-
+no outside resources
 ## MISCELLANEOUS COMMENTS:
  < Anything you would like the grader to know >
--- main.py
+++ main.py
@@ -1,18 +1,47 @@
-
+import turtle
 def seq3np1(n):
     """
         Print the 3n+1 sequence from n, terminating when it reaches 1.
         args: n (int) starting value for 3n+1 sequence
         return: None
     """
+    count = 0
     while(n != 1):
-        print(n)
+        count += 1
         if(n % 2) == 0:        # n is even
             n = n // 2
         else:                 # n is odd
             n = n * 3 + 1
-    print(n)                  # the last print is 1
+    return count                  # the last print is 1
+
+def graphData(upperBound = 0):
+  turtle.Screen()
+  todd = turtle.Turtle()
+  bob = turtle.Turtle()
+  turtle.setworldcoordinates(0, 0, 10, 10)
+  max_so_far = 0
+  for start in range(1, upperBound+1):
+    result = seq3np1(start)
+    if result > max_so_far:
+      max_so_far = result
+    todd.goto(0, max_so_far)
+    printString = ('Maximum so far: ', start, max_so_far)
+    todd.clear()
+    todd.write(printString, False)
+    turtle.setworldcoordinates(0,0,start+10, max_so_far+10)
+    bob.down
+    bob.goto(start, result)
+    bob.up
+  turtle.exitonclick()
 
 def main():
-	seq3np1(3)
+  upper_bound = int(input("Please input an upper bound: "))
+  if upper_bound > 0:
+    for start in range(1, upper_bound+1): 
+      result = seq3np1(start)
+      print('Loop iteration: ', start, ' || Result: ', result)
+    graphData(upper_bound)
+    
+  return
+
 main()
